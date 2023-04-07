aaf3c8099ed4116c3d7925177ec38d08e0886c56
edwar4rd <edwar4rd@gmail.com>
2023-04-07 19:02:09 +0800

    loses life when you strike a pack of snack

--- game.js
+++ game.js
@@ -101,6 +101,7 @@ let factLength = []
 factTexts.forEach(text => factLength.push(text.length));
 
 let gameFactObjs = [];
+let gameNegObjs = [];
 let gameCanvas = document.getElementById("game-main-canvas");
 let gameCanvasContext = gameCanvas.getContext("2d");
 let lastFrameClick = null;
@@ -119,7 +120,6 @@ setInterval(() => {
     gameCanvasContext.strokeStyle = "rgb(255,30,20)";
     gameCanvasContext.lineWdith = 3;
     gameCanvasContext.font = "30px monospace";
-
     gameFactObjs.forEach(factObj => {
         if (factObj[5]) {
             gameCanvasContext.fillStyle = "rgb(255,30,20)";
@@ -133,63 +133,98 @@ setInterval(() => {
                 gameCanvasContext.fillText(factTexts[factObj[3]], -18 * factLength[factObj[3]] - 4, factObj[4] + 32, 80);
             }
             gameCanvasContext.restore();
+
             gameCanvasContext.save();
             gameCanvasContext.rotate(-factObj[2]);
             gameCanvasContext.strokeRect(-2, -factObj[4] + 1, 18 * factLength[factObj[3]] + 4, -32);
             gameCanvasContext.restore();
-
-            // let rectPoints = [
-                // [-2, factObj[4] - 1],
-                // [-2, 32 + factObj[4] - 1],
-                // [-2 + 18 * factLength[factObj[3]] + 4, factObj[4] - 1],
-                // [-2 + 18 * factLength[factObj[3]] + 4, 32 + factObj[4] - 1]
-            // ];
-            // rectPoints.forEach(point => {
-                // gameCanvasContext.fillRect(...scaleCoor(...rotateCoor(...point, factObj[2]), 1, -1), 10, 10);
-            // });
-
-
-            // gameCanvasContext.fillRect(factObj[4] * Math.cos(factObj[2] + Math.PI / 2), -factObj[4] * Math.sin(factObj[2] + Math.PI / 2), 10, 10);
-        } else {
-            gameCanvasContext.fillStyle = "rgb(30,20,255)";
-            gameCanvasContext.save();
-            gameCanvasContext.rotate(-factObj[2]);
-            //gameCanvasContext.strokeRect(-2, -factObj[4] + 1, 18 * factLength[factObj[3]] + 4, -32);
-            gameCanvasContext.restore();
-            // gameCanvasContext.fillRect(factObj[4] * Math.cos(factObj[2] + Math.PI / 2), -factObj[4] * Math.sin(factObj[2] + Math.PI / 2), 10, 10);
         }
     });
     gameCanvasContext.restore();
+
+    gameCanvasContext.save();
+    gameCanvasContext.translate(400, 400);
+    gameCanvasContext.scale(1/3, 1/3);
+    gameNegObjs.forEach(negObj => {
+        if(negObj[4]) {
+            gameCanvasContext.strokeStyle = "rgb(20,255,60)";
+            gameCanvasContext.lineWidth = 5;
+            gameCanvasContext.save();
+            gameCanvasContext.rotate(-negObj[2]);
+            gameCanvasContext.drawImage(greenImage, 0, -negObj[3]*3);
+            gameCanvasContext.restore();
+
+            gameCanvasContext.save();
+            gameCanvasContext.rotate(-negObj[2]);
+            gameCanvasContext.strokeRect(0, -negObj[3]*3 - 1, 200, 258);
+            gameCanvasContext.restore();
+
+            let rectPoints = [
+                [0, negObj[3]],
+                [0, negObj[3] - 258 / 3],
+                [0 + 200 / 3, negObj[3]],
+                [0 + 200 / 3, negObj[3] - 258 / 3],
+            ];
+            gameCanvasContext.scale(3, 3);
+            rectPoints.forEach(point => {
+                // gameCanvasContext.fillRect(...tranlateCoor(...scaleCoor(...rotateCoor(...point, negObj[2]), 1, -1), 0, 0), 10, 10);
+            });
+            gameCanvasContext.scale(1/3, 1/3);
+        }
+    });
+    gameCanvasContext.restore();
+
     gameCanvasContext.save();
     gameCanvasContext.translate(400, 800);
     while (previousClick.length > 0 && (previousClick[0][1] + 200 < Date.now())) {
         previousClick.shift();
     }
-
     previousClick.forEach(click => {
         gameCanvasContext.save();
         gameCanvasContext.rotate(-click[0]);
-        drawBeam(0.4);
+        drawBeam((Date.now() - click[1])/400);
         gameCanvasContext.restore();
     });
     if (lastFrameClick) {
+        gameCanvasContext.save();
         gameCanvasContext.rotate(-lastFrameClick[0])
         drawBeam(0);
         previousClick.push(lastFrameClick);
+        gameCanvasContext.restore();
         gameFactObjs.forEach(factObj => {
-            let rectPoints = [
-                [-2, factObj[4] - 1],
-                [-2, 32 + factObj[4] - 1],
-                [-2 + 18 * factLength[factObj[3]] + 4, factObj[4] - 1],
-                [-2 + 18 * factLength[factObj[3]] + 4, 32 + factObj[4] - 1]
-            ];
-            rectPoints.forEach(point => {
-                if(pointInBeam(lastFrameClick[0], 80, ...tranlateCoor(...rotateCoor(...point, factObj[2]), 0, 400))) {
-                    factObj[5] = false;
-                    point += 1;
-                }
-            });
+            if(factObj[5]) {
+                let rectPoints = [
+                    [-2, factObj[4] - 1],
+                    [-2, 32 + factObj[4] - 1],
+                    [-2 + 18 * factLength[factObj[3]] + 4, factObj[4] - 1],
+                    [-2 + 18 * factLength[factObj[3]] + 4, 32 + factObj[4] - 1]
+                ];
+                rectPoints.forEach(point => {
+                    if(pointInBeam(lastFrameClick[0], 80, ...tranlateCoor(...rotateCoor(...point, factObj[2]), 0, 400))) {
+                        factObj[5] = false;
+                        point += 1;
+                    }
+                });
+            }
         });
+        gameNegObjs.forEach(negObj => {
+            if(negObj[4]) {
+                let rectPoints = [
+                    [0, negObj[3]],
+                    [0, negObj[3] - 258 / 3],
+                    [0 + 200 / 3, negObj[3]],
+                    [0 + 200 / 3, negObj[3] - 258 / 3],
+                ];
+                rectPoints.forEach(point => {
+                    if(pointInBeam(lastFrameClick[0], 80, ...tranlateCoor(...rotateCoor(...point, negObj[2]), 0, 400))) {
+                        if(negObj[4]) {
+                            negObj[4] = false;
+                            life -= 1;
+                        }
+                    }
+                });
+            }
+        })
         lastFrameClick = null;
     }
     gameCanvasContext.restore();
@@ -199,19 +234,34 @@ setInterval(() => {
 
     // Update Objects
     gameFactObjs.forEach(factObj => {
-        factObj[4] += 3;//+ Math.random() * 2;
+        factObj[4] += 3 + Math.random() * 2;
     });
 
     while (gameFactObjs.length > 0 && (gameFactObjs[0][4] > 500)) {
         gameFactObjs.shift();
     }
-}, 10);
+
+    gameNegObjs.forEach(negObj => {
+        negObj[3] += 4 + Math.random() * 4;
+    });
+
+    while (gameNegObjs.length > 0 && (gameNegObjs[0][3] > 500)) {
+        gameNegObjs.shift();
+    }
+    
+}, 17);
 
 setInterval(() => {
-    if (Math.random() < 1 / 3) {
+    if (Math.random() < 0.5) {
         let newObject = [400, 400, Math.random() * 2.0 * Math.PI, Math.floor(Math.random() * factTexts.length), 0, true];
         gameFactObjs.push(newObject);
     }
+
+    if (Math.random() < 0.08) {
+        let newObject = [400, 400, Math.random() * 2.0 * Math.PI, Math.floor(Math.random() * factTexts.length), true];
+        gameNegObjs.push(newObject);
+    }
+    
 }, 50);
 
 addEventListener("click", e => {
